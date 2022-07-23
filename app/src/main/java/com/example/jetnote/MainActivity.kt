c667351279060c7b1404ee0b15778228a089e104
Qraksa <filipsztajn@gmail.com>
2022-07-23 17:54:08 +0100

    JetNote Beta
    Added:
    
    *function of saving and removing notes from list

--- app/src/main/java/com/example/jetnote/MainActivity.kt
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -8,10 +8,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnote.data.NotesDataSource
+import com.example.jetnote.model.Note
+import com.example.jetnote.screen.NoteScreen
 import com.example.jetnote.ui.theme.JetNoteTheme
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +29,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val notes = remember {
+                        mutableStateListOf<Note>()
+                    }
+                    NoteScreen(notes = notes,
+                        onRemoveNote = {
+                                       notes.remove(it)
+                        },
+                        onAddNote = {
+                            notes.add(it)
+                        })
                 }
+
             }
         }
     }
 }
 
 
+@ExperimentalComposeUiApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetNoteTheme {
-
+        val notes = remember {
+            mutableStateListOf<Note>()
+        }
+        NoteScreen(notes = notes,
+            onRemoveNote = {
+                notes.remove(it)
+            },
+            onAddNote = {
+                notes.add(it)
+            })
     }
 }
